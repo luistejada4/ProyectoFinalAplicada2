779b222a0a1fdac3db6dda41058420ca2d7594e8
Luis Antonio Tejada Acevedo <luis_tejada4@ucne.edu.do>
2017-07-31 12:40:10 -0400

    Initial

---
+++ ProyectoFinalAplicada2/Models/Boleta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Boleta
+    {
+        public int Id { get; set; }
+        public Evento Evento{ get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Boleta()
+        {
+
+        }
+    }
+}
---
+++ ProyectoFinalAplicada2/Models/Evento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public List<Boleta> Boletas { get; set; }
+
+        public Evento()
+        {
+
+        }
+
+    }
+}
