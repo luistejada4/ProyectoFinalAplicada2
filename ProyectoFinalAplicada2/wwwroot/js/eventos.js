f7a3c61373cb81489d96da212fc20d340d91d930
Luis Antonio Tejada Acevedo <luis_tejada4@ucne.edu.do>
2017-08-02 11:58:41 -0400

    Add litle angular code

--- ProyectoFinalAplicada2/wwwroot/js/eventos.js
+++ ProyectoFinalAplicada2/wwwroot/js/eventos.js
@@ -1,18 +1,18 @@
 ﻿var app = angular.module("App", []);
 
-app.controller("EventosController", function ($scope, $http) {
+app.controller("EventosController", function ($scope, $http, $window) {
 
     $scope.eventos = [];
-
-
+    $scope.master = true;
 
     var getEventos = function () {
 
-        $http.get("/api/Eventos/ ").then(
+        $http.get("/api/Eventos/").then(
 
             function (response) {
 
-               alert(response.data[0].descripcion);
+                $scope.eventos = response.data;
+                
 
             }, function (response) {
 
@@ -22,9 +22,15 @@ app.controller("EventosController", function ($scope, $http) {
     };
 
 
+    $scope.eventoClicked = function (cliente) {
+
+        alert("Hola");
+        $window.location.href = '/eventos/details/' + cliente.id;
+    };
+
+
 
     getEventos();
-    alert($scope.eventos.length);
 
 
 });
